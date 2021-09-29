8e822b849db96ae57a028bdf413e385ee44e2299
John Doe <johndoe@example.com>
2021-09-29 21:24:42 +0400

    some

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -20,14 +20,9 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(width, height) {
-  this.width = width;
-  this.height = height;
-  Rectangle.prototype.getArea = function () {
-    return this.width * this.height;
-  };
+function Rectangle(/* width, height */) {
+  throw new Error('Not implemented');
 }
-
 
 /**
  * Returns the JSON representation of specified object
